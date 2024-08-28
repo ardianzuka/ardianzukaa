1c0a90030eadc2027744049c8d2d092ef0e1d76f
ardian zuka <ardianzuka3@gmail.com>
2024-08-28 22:09:42 +0200

    ardianzuka

---
+++ DataTypes.java
@@ -0,0 +1,75 @@
+package S2;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // Integer types
+        //byte(8-bit signed integer)
+
+        byte numberOfFloors = 16;
+        byte usbPortsnumber = 3;
+        byte noOfWheels = 4;
+        byte noOfDays = 7;
+        byte noOfClasses = 0;
+
+        String seperator = "\n\n======================";
+        System.out.println(seperator);
+        System.out.println("Number of floors: " + numberOfFloors);
+        System.out.println("Number of Classes: " + noOfClasses);
+
+        System.out.println("\n\n\n");
+
+        //short: (16-bit signed integer)
+        short noOfPopulation = 32767;
+        short noOfEmployees = 500;
+        short noOfStudents = 875;
+        short noOFpages = 287;
+
+        // int: (32-bit integer)
+        int marathonDistance = 42195;
+        int populationOfLargecity = 1000023;
+        int earthToMarsDistanceInKm = 22554305;
+        int secondsInYear = 324566000;
+        int numberOfView = 100030000;
+        int debtInBillions = 1020304000;
+
+        // long: (64-bit integer)
+        long worldPopulation = 789999490L;
+        long starsInMilkyWay = 2500000400L;
+        long distanceToObserveableGalaxyEdge = 30203049596030L;
+
+        // floating-point types
+        // float: (32-bit single-precision floating- point)
+        float avgHumanBodyTemp = 36.5f;
+        float priceOfProduct = 23.25f;
+        float avgGrade = 10.0f;
+        float packageWeight = 4.55f;
+
+        //double: (64-bit double-precision floating-point)
+        double longitude = 37.2343;
+        double latitude = 122.2323;
+        double aotmicWeightCarbon = 12.0107;
+        double goldenRatio = 1.6180405060;
+
+        //char: (16-bit unicode charaacter)
+        char firstLetterOfName = 'Q';
+        char currencySymbol = '$';
+        char gradeA = 'A';
+        char plusOperator = '+';
+
+        // bolean: (1-bit true or false value)
+        boolean switchOnOrOff = false;
+        boolean isUserLoggedIn = true;
+        boolean isAvailable = true;
+        boolean isCompleted = true;
+
+
+
+
+
+
+
+
+    }
+}
